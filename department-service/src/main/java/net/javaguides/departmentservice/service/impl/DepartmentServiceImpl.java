2459c7c1780ddce4bcf861f8346cfba5d5899ee5
keremcengiz0 <keremcengiz0@hotmail.com>
2023-06-12 14:34:41 +0300

    added department controller and service.

---
+++ department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,38 @@
+package net.javaguides.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.departmentservice.dto.DepartmentDto;
+import net.javaguides.departmentservice.entity.Department;
+import net.javaguides.departmentservice.repository.DepartmentRepository;
+import net.javaguides.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+
+        return savedDepartmentDto;
+    }
+}
