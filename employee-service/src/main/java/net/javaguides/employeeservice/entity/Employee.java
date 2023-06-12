e37ee46cb1158554d6d00e2e94c694b97397d02f
keremcengiz0 <keremcengiz0@hotmail.com>
2023-06-12 15:44:44 +0300

    added entity and db configs for employee-service module.

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/entity/Employee.java
@@ -0,0 +1,23 @@
+package net.javaguides.employeeservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
