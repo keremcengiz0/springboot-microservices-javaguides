6d59b1ea5e59522a5c774a45728421ddd2df0d92
keremcengiz0 <keremcengiz0@hotmail.com>
2023-06-20 17:11:11 +0300

    upgraded spring boot and spring cloud versions.

--- organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
@@ -2,10 +2,9 @@ package net.javaguides.organizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
